6162e6198f60212239bdd216f68a8d24558ecdd1
mahimabhimwani <91219547+mahimabhimwani@users.noreply.github.com>
2022-05-23 16:08:49 +0530

    Update RnaTranscription.java

--- exercises/practice/rna-transcription/src/main/java/RnaTranscription.java
+++ exercises/practice/rna-transcription/src/main/java/RnaTranscription.java
@@ -1,7 +1,30 @@
 class RnaTranscription {
 
     String transcribe(String dnaStrand) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+      //  int n = dnaStrand.length();
+        char ch[] = dnaStrand.toCharArray();
+        String a ="";
+        for(int i=0;i<ch.length;i++){
+         if(ch[i]=='G'){
+             ch[i]='C';
+         }
+            else if(ch[i]=='C'){
+                ch[i]='G';
+            }
+              else if(ch[i]=='T'){
+                ch[i]='A';
+            }
+              else if(ch[i]=='A'){
+                ch[i]='U';
+            }
+                  
+            }
+        // for(int i =0; i < ch.length;i++){
+        //     a = a +ch[i];
+            
+        // }
+        
+        return String.copyValueOf(ch);
+        
 
 }
