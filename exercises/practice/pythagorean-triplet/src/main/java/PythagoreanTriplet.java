52bc03063e6ff75f8fab4242a02f42560f3a658e
mahimabhimwani <91219547+mahimabhimwani@users.noreply.github.com>
2022-05-23 16:10:55 +0530

    Update PythagoreanTriplet.java

--- exercises/practice/pythagorean-triplet/src/main/java/PythagoreanTriplet.java
+++ exercises/practice/pythagorean-triplet/src/main/java/PythagoreanTriplet.java
@@ -8,3 +8,19 @@ which is an important part of programming in Java.
 Please remove this comment when submitting your solution.
 
 */
+public class PythagoreanTripletTest{
+    public int pythagoreanTriplet(int N){
+        int count=0;
+        for(int i = 1;i <= N/3;i++){
+            for(int j=i+1;j<=N/2;j++){
+                int k=N-i-j;
+               
+                    if( (i*i)+(j*j)== k*k){
+                        count++;
+                    }
+                
+            }
+        }
+        return count;
+    }
+}
