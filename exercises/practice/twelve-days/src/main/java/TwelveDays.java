262c10f8d122be6d153a8c775dfaa8dc9ba03846
mahimabhimwani <91219547+mahimabhimwani@users.noreply.github.com>
2022-05-24 20:05:26 +0530

    Update TwelveDays.java

--- exercises/practice/twelve-days/src/main/java/TwelveDays.java
+++ exercises/practice/twelve-days/src/main/java/TwelveDays.java
@@ -1,13 +1,56 @@
 class TwelveDays {
+    private String[] dayCount = {
+            "first", "second", "third", "fourth", "fifth", "sixth",
+            "seventh", "eighth", "ninth", "tenth", "eleventh", "twelfth"
+    };
+    private String[] allphrases = {
+            "a Partridge in a Pear Tree",
+            "two Turtle Doves",
+            "three French Hens",
+            "four Calling Birds",
+            "five Gold Rings",
+            "six Geese-a-Laying",
+            "seven Swans-a-Swimming",
+            "eight Maids-a-Milking",
+            "nine Ladies Dancing",
+            "ten Lords-a-Leaping",
+            "eleven Pipers Piping",
+            "twelve Drummers Drumming"
+    };
+    private String mainPhrasePartOne = "On the ";
+    private String mainPhrasePartTwo = " day of Christmas my true love gave to me: ";  
+    
     String verse(int verseNumber) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        StringBuilder p = new StringBuilder();
+        
+        p.append(mainPhrasePartOne);
+        p.append(dayCount[verseNumber - 1]);
+        p.append(mainPhrasePartTwo);
+        
+        for (int i = verseNumber; i > 0; i--) {
+            p.append(allphrases[i - 1]);
+            if (i != 1) {
+                p.append(", ");
+            }
+            if (i == 2) {
+                p.append("and ");
+            }
+        }
+        p.append(".\n");
+        return p.toString();
     }
-
+    
     String verses(int startVerse, int endVerse) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        StringBuilder p = new StringBuilder();
+        for (int i = startVerse; i <= endVerse; i++) {
+            p.append(verse(i));
+            if (i != endVerse) p.append("\n");
+        }
+        return p.toString();
     }
     
     String sing() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return verses(1, 12);
     }
 }
+
